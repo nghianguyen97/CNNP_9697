c1a7b9a81fa79896f6f6e0e641bdbb7a7ed9a492
Phat <kingdragontp2@gmail.com>
2019-08-21 17:06:17 +0700

    update

--- Server_Nodejs/api/controllers/StudentController.js
+++ Server_Nodejs/api/controllers/StudentController.js
@@ -1,7 +1,9 @@
 import 'dotenv/config';
+const request = require("request")
 const generateToken = require("../generateToken");
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
+//abc
 module.exports = {
     get: (req, res) => {
         generateToken((result, err) => {
