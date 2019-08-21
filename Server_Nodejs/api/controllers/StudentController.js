63e96528e3c2e8413e09a788c7065da6fe567aaa
Phat <kingdragontp2@gmail.com>
2019-08-21 17:16:07 +0700

    update

--- Server_Nodejs/api/controllers/StudentController.js
+++ Server_Nodejs/api/controllers/StudentController.js
@@ -3,12 +3,6 @@ const request = require("request")
 const generateToken = require("../generateToken");
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-<<<<<<< HEAD
-//hello
-//hi
-=======
-//abc
->>>>>>> way2
 module.exports = {
     get: (req, res) => {
         generateToken((result, err) => {
