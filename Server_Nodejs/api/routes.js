7f027b2e6172361d0b045c5c9aae234db5794a1e
nghianguyen97 <nghia.nguyen.set15@eiu.edu.vn>
2019-08-21 15:53:14 +0700

    Generate Token

---
+++ Server_Nodejs/api/routes.js
@@ -0,0 +1,10 @@
+const studentControler = require("./controllers/StudentController");
+const cors = require('cors');
+
+
+module.exports = (app) => {
+    app.use(cors());
+    app.route('/student')
+        .get(studentControler.get);
+
+};
---
+++ Server_Nodejs/server.js
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+let routes = require('./api/routes') //importing route
+routes(app)
+
+app.use(function (req, res) {
+    res.status(404).send({ url: req.originalUrl + ' not found' })
+})
+
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Express running → PORT ${server.address().port}`),
+);
